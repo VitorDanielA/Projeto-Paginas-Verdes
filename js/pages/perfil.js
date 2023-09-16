a141720c73e2fdcf675cf1fbbe3eaa0f6931d0c9
tarciiz <sidtarcisiosid88@gmail.com>
2023-09-16 13:16:49 -0300

    Atualizado para receber a imagem

--- js/pages/perfil.js
+++ js/pages/perfil.js
@@ -4,7 +4,12 @@ $("#perfil_email").html(getUser().email);
 $("#perfil_telefone").html(getUser().phone);
 
 let add = getUser().address
-$("#perfil_endereco").html(add.street +', ' + add.number + ', ' + add.city + ' - ' + add.state);
+$("#perfil_endereco").html(add.street||'' +', ' + add.number||'' + ', ' + add.city||'' + ' - ' + add.state||'');
 
 $("#empresa_cidade").html('IFBA, ' + add.city + ' - ' + add.state);
+
+
+console.log('User ', getUser())
+
+if(getUser().profilePicture.link ) {$("#profile_picture").attr("src", getUser().profilePicture.link );}
 
